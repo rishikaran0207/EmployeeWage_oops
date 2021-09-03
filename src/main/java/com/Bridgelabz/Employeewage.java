7436a091b7a91e4ce54be6e22e0ead4524af6e47
rishikaran0207 <sendittomemrk@gmail.com>
2021-09-03 19:33:12 +0530

    Added: Part time emp

--- src/main/java/com/Bridgelabz/Employeewage.java
+++ src/main/java/com/Bridgelabz/Employeewage.java
@@ -1,30 +1,52 @@
 package com.Bridgelabz;
 public class Employeewage {
     public static int punch=(int)(Math.random()*2);
-    public static int WAGE_PER_HOUR = 20;
+    int Is_fulltime = (int) (Math.floor(Math.random() * 8 % 9) + 1);
+    int Is_partime = (int) (Math.floor(Math.random() * 4 % 5) + 1);
+    public static int Per_Hrs_Pay = 20,Full_Day = 8,Partime_Hrs=4;
 
-    public static void empPresentorNot()
+
+    public void fulltime_emp_PresentorAbesnt()
     {
-        if(punch== 1)
+        System.out.println("\n\n Fulltime employee present or absent:");
+        empPresentorNot();
+        employeewage(Full_Day,Is_fulltime);
+    }
+
+    public void parttime_employeewage()
+    {
+        System.out.println("\n\n Parttime employee present or absent:");
+        empPresentorNot();
+        employeewage(Partime_Hrs,Is_partime);
+
+    }
+
+    public void empPresentorNot()
+    {
+        if(punch == 1)
             System.out.println("\n  employee is present today");
         else
             System.out.println("\n  employee is absent today");
     }
 
-    public void empWage(int hour,int present)
+
+    public void employeewage(int hrs,int present)
     {
-        System.out.println("\n\ncheck the employee daily wage:");
-        int fullpresent = WAGE_PER_HOUR * hour;
-        System.out.println("\n  if employee worker is present for full hour, the salary is " + fullpresent);
+        //System.out.println("\n\n the employee wage is:");
+        int fullpresent = Per_Hrs_Pay * hrs;
+        System.out.println("\n The salary is " + fullpresent);
         if (punch == 1) {
-            int hoursbased = present * WAGE_PER_HOUR;
-            System.out.println("\n  employee's salary based on hours, employee present for " + present + " hours so his salary is " + hoursbased);
+            int hoursbased = present * Per_Hrs_Pay;
+            System.out.println("\n  Employee present for " + present + " hrs. Salary: " + hoursbased);
         } else
-            System.out.println("\n  the employee's salary is 0, because employee is absent");
+            System.out.println("\n  Employee is absent");
     }
 
     public static void main(String[] args) {
         System.out.println("\nWelcome to Employee Wage using OOPS");
+        Employeewage emp=new Employeewage();
+        emp.fulltime_emp_PresentorAbesnt();
+        emp.parttime_employeewage();
     }
 }
 
