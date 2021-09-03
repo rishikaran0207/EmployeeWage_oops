2183fc437ae7f48b6487fbff18233bb86b072be9
rishikaran0207 <sendittomemrk@gmail.com>
2021-09-03 19:02:06 +0530

    Added: Employee is present or Absent

--- src/main/java/com/Bridgelabz/Employeewage.java
+++ src/main/java/com/Bridgelabz/Employeewage.java
@@ -1,8 +1,17 @@
 package com.Bridgelabz;
 public class Employeewage {
+    public static int punch=(int)(Math.random()*2);
+    public static void empPresentorNot()
+    {
+        if(punch== 1)
+            System.out.println("\n  employee is present today");
+        else
+            System.out.println("\n  employee is absent today");
+    }
+
     public static void main(String[] args) {
         System.out.println("\nWelcome to Employee Wage using OOPS");
-
+        empPresentorNot();
     }
 }
 
